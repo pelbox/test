aedd2f015f05fa775c40eba38b44e846b24d0abf
Pelbox Solutions <gagandeep@pelbox.com>
2024-02-25 11:28:44 +0000

    forst commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function RGBcolor() {
+    var R = Math.floor(Math.random() * 256);
+    var G = Math.floor(Math.random() * 256);
+    var B = Math.floor(Math.random() * 256);
+    var randomcolor = "rgb(" + R + "," + G + "," + B + ")";  
+    console.log(randomcolor);
+  }
+  
+  RGBcolor();
